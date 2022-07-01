aff4be1d003660971d8877bc43b996175db096eb
Aaron Panchal <aaron.panchal@gmail.com>
2022-07-01 15:15:21 -0700

    Override default nextjs document
    - Extract favicon stuff

--- pages/index.js
+++ pages/index.js
@@ -9,31 +9,6 @@ export default function Home() {
     <div>
       <Head>
         <title>Aaron Panchal | Film Photography</title>
-
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="favicon-32x32.png"
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="favicon-16x16.png"
-          sizes="16x16"
-        />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="mask-icon" href="safari-pinned-tab.svg" color="#f9886c" />
-        <meta name="theme-color" content="#ffffff" />
-        <meta
-          name="description"
-          content="Aaron Panchal's photography portfolio"
-        />
       </Head>
 
       <div className={styles.layout}>
